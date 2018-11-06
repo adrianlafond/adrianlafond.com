d3e2a05828b5f1993cf325d2025e7e8ef27588b8
Adrian Lafond <mail@adrianlafond.com>
2018-11-06 18:01:47 -0500

    draw simple shapes at dpr 2

--- _javascript/b/1/index.js
+++ _javascript/b/1/index.js
@@ -1,18 +1,57 @@
 import Bug from '../../main/bug';
 import { bootstrapElement } from '../../main/bootstrap';
 
-function B1(hostElement) {
-  const renderType = PIXI.utils.isWebGLSupported() ? 'WebGL' : 'canvas';
-  PIXI.utils.sayHello(renderType);
-  const app  = new PIXI.Application({
-    width: 320,
-    height: 320,
-    antialias: true,
-    resolution: 1,
-  });
-  app.renderer.backgroundColor = 0xf5f5dc;
-  app.renderer.autoResize = true;
-  hostElement.appendChild(app.view);
+class B1 {
+  constructor(hostElement) {
+    this.hostElement = hostElement;
+    this.init();
+  }
+
+  init() {
+    this.createApp();
+    this.addTick();
+    this.drawShapes();
+  }
+
+  createApp() {
+    const renderType = PIXI.utils.isWebGLSupported() ? 'WebGL' : 'canvas';
+    PIXI.utils.sayHello(renderType);
+    this.app = new PIXI.Application({
+      width: 320,
+      height: 320,
+      antialias: true,
+      resolution: 2,
+    });
+    this.app.renderer.backgroundColor = 0xf5f5dc;
+    this.app.renderer.autoResize = true;
+    this.hostElement.appendChild(this.app.view);
+  }
+
+  addTick() {
+    this.ticking = false;
+    const tickFn = delta => { this.onTick(delta); }
+    this.hostElement.addEventListener('mousedown', () => {
+      if (this.ticking) {
+        this.app.ticker.add(tickFn);
+      } else {
+        this.app.ticker.remove(tickFn);
+      }
+      this.ticking = !this.ticking;
+    });
+  }
+
+  drawShapes() {
+    const circle = new PIXI.Graphics();
+    circle.lineStyle(1, 0x000000, 1);
+    circle.drawCircle(0, 0, 3);
+    circle.x = 100;
+    circle.y = 100;
+    this.app.stage.addChild(circle);
+  }
+
+  onTick(delta) {
+    console.log(delta);
+  }
 }
 
 const hostElement = bootstrapElement();
